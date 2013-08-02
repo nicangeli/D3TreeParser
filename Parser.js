31103f7185d9d76a733fd01f3a6675b6471911fc
Nicholas Angeli <nicangeli@gmail.com>
2013-08-02 22:03:59 +0100

    text file now supports multiple details

--- Parser.js
+++ Parser.js
@@ -18,14 +18,17 @@ function Parser() {
 		var root;
 		var previousLength = 1; 
 		for(var i = 0; i < paths.length; i++) {
+
 			if(paths[i].length == 1) { // this is the root node
-				root = {name: paths[i][0], children: []}
+				var details = paths[i][0].split("|");
+				root = {name: details[0], title: details[1], wte: details[2], children: []}
 				current_at_depth[0] = root;
 				previousLength = 1;
 			} else {
 				var currentLength = paths[i].length;
 				var parent = current_at_depth[currentLength-2];
-				var tmp = {name: paths[i][currentLength-1], children: []};
+				var full = paths[i][currentLength-1].split("|");
+				var tmp = {name: full[0], title: full[1], wte: details[2], children: []};
 				parent.children.push(tmp);
 				current_at_depth[currentLength-1] = tmp;
 				previousLength = currentLength;
